31df09369009ae65541c6a8080e73f246a7578f8
준서 <jsfun413@naver.com>
2022-05-06 01:22:52 +0900

    Reset
    Maybe

--- EGG-AP.py
+++ EGG-AP.py
@@ -5,19 +5,20 @@ import time
 # Arduino Set
 port = "COM5"
 baud = "230400"
+# Serial Connection
 ser = serial.Serial(port, baud, timeout=1)
 
 # Threading
 def main():
-    thread = threading.Thread(target=Input, args=(ser,))
+    thread = threading.Thread(target=Connection, args=(ser,))
     thread.start()
 
 # Buffer Input
-def Input(ser):
+def Connection(ser):
     while True:
         if ser.readable():
             res = ser.readline()
-            print(res)
+            print(res.decode()[:len(res)])
     
     ser.close()
 
