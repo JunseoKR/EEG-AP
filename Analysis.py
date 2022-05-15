78387340f8516b47d1d2a51a56b27f7d6f2dc0df
준서 <jsfun413@naver.com>
2022-05-15 13:42:11 +0900

    Alpha / Beta Range Check

--- Analysis.py
+++ Analysis.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Feb 2021
-
-@author: Natalia
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as waves
+from scipy import signal
 import csv
 import pandas as pd
 
+
+
+
+
+# ---------------------------------------------------------------------
+# EEG Main Set
+
+# 파일 불러오기
 file = r'C:\\Users\\Develop\\Documents\\BYB\\Sample.wav'
+# scipy.io.wavefile 모듈
 fs, data = waves.read(file)
 
+# numpy 차원 재배열
 length_data=np.shape(data)
 length_new=length_data[0]*0.05
 ld_int=int(length_new)
-from scipy import signal
-data_new=signal.resample(data,ld_int)
 
+# scipy 데이터 resample
+data_new=signal.resample(data, ld_int)
+
+# Matplot 그래프 생성
 plt.figure('Spectrogram')
 d, f, t, im = plt.specgram(data_new, NFFT= 256, Fs=500, noverlap=250)
 plt.ylim(0,90)
@@ -28,18 +36,30 @@ plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [s]')
 plt.show()
 
+
+
+
+
+# ---------------------------------------------------------------------
+# Alpha / Beta Range Check
+
+# Frequencies CSV 생성
 matrixf=np.array(f).T
 np.savetxt('Frequencies.csv', matrixf)
 df = pd.read_csv("Frequencies.csv", header=None, index_col=None)
 df.columns = ["Frequencies"]
 df.to_csv("Frequencies.csv", index=False)
 
+# 파형 주파수 선택
 position_vector=[]
 length_f=np.shape(f)
 l_row_f=length_f[0]
 for i in range(0, l_row_f):
-    if f[i]>=7 and f[i]<=12:
+    if f[i]>=20 and f[i]<=30:
         position_vector.append(i)
+
+print(position_vector)
+
 
 length_d=np.shape(d)
 l_col_d=length_d[1]
@@ -47,7 +67,7 @@ AlphaRange=[]
 for i in range(0,l_col_d):
     AlphaRange.append(np.mean(d[position_vector[0]:max(position_vector)+1,i]))
 
-
+# Savitzky-Golay 노이즈 제거
 def smoothTriangle(data, degree):
     triangle=np.concatenate((np.arange(degree + 1), np.arange(degree)[::-1])) # up then down
     smoothed=[]
@@ -61,11 +81,13 @@ def smoothTriangle(data, degree):
         smoothed.append(smoothed[-1])
     return smoothed
 
+# Matplot 그래프 생성
 plt.figure('AlphaRange')
 y=smoothTriangle(AlphaRange, 100)
 plt.plot(t, y)
 plt.xlabel('Time [s]')
 plt.xlim(0,max(t))
+plt.show()
 
 datosy=np.asarray(y)
 datosyt=np.array(
@@ -77,12 +99,17 @@ with open ('datosyt.csv', 'w', newline='') as file:
     writer=csv.writer(file, dialect='excel-tab')
     writer.writerows(datosyt.T)
     
-
+# CSV 헤더 생성
 df = pd.read_csv("datosyt.csv", header=None, index_col=None)
 df.columns = ["Power                   Time"]
 df.to_csv("datosyt.csv", index=False)
 
 
+
+
+
+# ---------------------------------------------------------------------
+# Eyes Closed / Eyes Open
 
 tg=np.array([4.2552,14.9426, 23.2801,36.0951, 45.4738,59.3751, 72.0337,85.0831, max(t)+1])
 
@@ -107,6 +134,7 @@ plt.figure('DataAnalysis')
 plt.boxplot([eyesopen, eyesclosed], sym = 'ko', whis = 1.5)
 plt.xticks([1,2], ['Eyes open', 'Eyes closed'], size = 'small', color = 'k')
 plt.ylabel('AlphaPower')
+#plt.show()
 
 meanopen=np.mean(eyesopen)
 meanclosed=np.mean(eyesclosed)
