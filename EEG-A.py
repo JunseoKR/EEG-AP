9375aa5e4f851e30db4938b84309cae64e575950
준서 <jsfun413@naver.com>
2022-07-19 13:16:24 +0900

    Recontextualize

--- EEG-A.py
+++ EEG-A.py
@@ -22,7 +22,12 @@ class Form():
 Grouping = 100
 
 # Wave Range
-WaveRange = "Alpha"
+WaveRange1 = "Delta"
+WaveRange2 = "Theta"
+R1_Initial_Hz = 0.2
+R1_End_Hz = 4
+R2_Initial_Hz = 4
+R2_End_Hz = 8
 
 # WaveFiles
 WAVFile = [
@@ -44,31 +49,27 @@ for n in range(1, WAVNUM):
     eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im = Sampling_Process(eval('WAVData_'+str(n)))
     
     # FFT Process
-    # Alpha
-    eval('S'+str(n)).y1 = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, 8, 12)
-    eval('S'+str(n)).y2 = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, 20, 30)
+    eval('S'+str(n)).y1 = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, R1_Initial_Hz, R1_End_Hz)
+    eval('S'+str(n)).y2 = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, R2_Initial_Hz, R2_End_Hz)
 
     # Data Process
-    #DATA_Process(eval('S'+str(n)).y1, eval('S'+str(n)).t, WAVFile[n], Grouping, "Alpha")
-    #DATA_Process(eval('S'+str(n)).y2, eval('S'+str(n)).t, WAVFile[n], Grouping, "Beta")
+    DATA_Process(eval('S'+str(n)).y1, eval('S'+str(n)).t, WAVFile[n], Grouping, WaveRange1)
+    DATA_Process(eval('S'+str(n)).y2, eval('S'+str(n)).t, WAVFile[n], Grouping, WaveRange2)
 
 
-GraphColor = ["red", "orange", "yellow", "green", "blue", "pink", "purple"]
-plt.figure('{} Range'.format(WaveRange))
+
+# matplotlib Graph
+plt.figure('{} / {} Range'.format(WaveRange1, WaveRange2))
 plt.xlabel('Time [s]')
-plt.ylabel('{} Power'.format((WaveRange)))
+plt.ylabel('{} / {} Power'.format((WaveRange1, WaveRange2)))
 #plt.xlim(0,max(t1))
 plt.ylim(0, 2000)
 
 # Plot Section
-plt.plot(S1.t, S1.y1, label='S3_A1_A', color=GraphColor[0])
-plt.plot(S1.t, S1.y2, label='S3_A1_B', color=GraphColor[1])
-plt.plot(S2.t, S2.y1, label='S3_B1_A', color=GraphColor[2])
-plt.plot(S2.t, S2.y2, label='S3_B1_B', color=GraphColor[3])
+plt.plot(S1.t, S1.y1, label='S3_A1_D')
+plt.plot(S1.t, S1.y2, label='S3_A1_T')
+plt.plot(S2.t, S2.y1, label='S3_B1_D')
+plt.plot(S2.t, S2.y2, label='S3_B1_T')
 
 plt.legend()
 plt.show()
-
-# Plot Multi Call
-# for g in range(1, WAVNUM):
-#     plt.plot(eval('S'+str(n)).t, eval('S'+str(n)).y, color=GraphColor[g-1])
--- EEG-MA.py
+++ EEG-MA.py
@@ -22,12 +22,25 @@ class Form():
 Grouping = 100
 
 # Wave Range
-WaveRange = ""
+WaveRange = "Alpha"
+Initial_Hz = 8
+End_Hz = 13
 
 # WaveFiles
 WAVFile = [
     "Empty",
-    "S1_A1"
+    "S1_B1",
+    "S1_B2",
+    "S1_B3",
+    "S2_B1",
+    "S2_B2",
+    "S2_B3",
+    "S3_B1",
+    "S3_B2",
+    "S3_B3",
+    "S4_B1",
+    "S4_B2",
+    "S4_B3",
 ]
 WAVNUM = len(WAVFile)
 
@@ -43,13 +56,14 @@ for n in range(1, WAVNUM):
     eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im = Sampling_Process(eval('WAVData_'+str(n)))
     
     # FFT Process
-    eval('S'+str(n)).y = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, 0.2, 4)
+    eval('S'+str(n)).y = FFT_Process(eval('S'+str(n)).d, eval('S'+str(n)).f, eval('S'+str(n)).t, eval('S'+str(n)).im, Initial_Hz, End_Hz)
 
     # Data Process
-    DATA_Process(eval('S'+str(n)).y, eval('S'+str(n)).t, WAVFile[n], Grouping, "Delta")
+    DATA_Process(eval('S'+str(n)).y, eval('S'+str(n)).t, WAVFile[n], Grouping, WaveRange)
 
 
-GraphColor = ["red", "orange", "yellow", "green", "blue", "pink", "purple"]
+
+# matplotlib Graph
 plt.figure('{} Range'.format(WaveRange))
 plt.xlabel('Time [s]')
 plt.ylabel('{} Power'.format((WaveRange)))
@@ -58,22 +72,17 @@ plt.ylim(0, 2000)
 
 # Plot Section
 plt.plot(S1.t, S1.y, label='S1_B1')
-# plt.plot(S2.t, S2.y, label='S1_B2')
-# plt.plot(S3.t, S3.y, label='S1_B3')
-# plt.plot(S4.t, S4.y, label='S2_B1')
-# plt.plot(S5.t, S5.y, label='S2_B2')
-# plt.plot(S6.t, S6.y, label='S2_B3')
-# plt.plot(S7.t, S7.y, label='S3_B1')
-# plt.plot(S8.t, S8.y, label='S3_B2')
-# plt.plot(S9.t, S9.y, label='S3_B3')
-# plt.plot(S10.t, S10.y, label='S4_B1')
-# plt.plot(S11.t, S11.y, label='S4_B2')
-# plt.plot(S12.t, S12.y, label='S4_B3')
-
+plt.plot(S2.t, S2.y, label='S1_B2')
+plt.plot(S3.t, S3.y, label='S1_B3')
+plt.plot(S4.t, S4.y, label='S2_B1')
+plt.plot(S5.t, S5.y, label='S2_B2')
+plt.plot(S6.t, S6.y, label='S2_B3')
+plt.plot(S7.t, S7.y, label='S3_B1')
+plt.plot(S8.t, S8.y, label='S3_B2')
+plt.plot(S9.t, S9.y, label='S3_B3')
+plt.plot(S10.t, S10.y, label='S4_B1')
+plt.plot(S11.t, S11.y, label='S4_B2')
+plt.plot(S12.t, S12.y, label='S4_B3')
 
 plt.legend()
 plt.show()
-
-# Plot Multi Call
-# for g in range(1, WAVNUM):
-#     plt.plot(eval('S'+str(n)).t, eval('S'+str(n)).y, color=GraphColor[g-1])
